f3a18a6cb498bbd5ff33d8ea97ece4d3ba4dd243
Madhavan-60 <madhavankannan73@gmail.com>
2025-12-23 12:44:13 +0530

    update

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import trackRoutes from "./src/controllers/config/routes/trackRoutes.js";
+import trackRoutes from "./src/routes/trackRoutes.js";
 
 dotenv.config();
 
@@ -17,6 +17,6 @@ app.get("/", (req, res) => {
 });
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () =>
-  console.log(`Server running on port ${PORT}`)
-);
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
